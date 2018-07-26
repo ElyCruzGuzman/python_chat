f363decd8d3e15973b9bbd0df3cfd79547db9049
Javier Aguirre <contacto@javaguirre.net>
2018-07-26 19:00:38 +0200

    Fix function names

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
     return "Hola Mundo!"
 
 
 @app.route("/contact")
-def hello():
+def contact():
     return "Si necesitas algo, enviame un mail a @example.com"
